ff1d2628328624e82bb342c6022f1d3699c40c18
Gulshan Kumar <innoventes@Innoventes-Technologies.local>
2020-07-30 18:11:07 +0530

    fix

--- src/App.js
+++ src/App.js
@@ -2,13 +2,11 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Routing from "../src/routing"
-import Notification from "./components/Notification"
 function App() {
   return (
     <div className='App'>
       <Router>
         <Routing />
-        <Notification />
       </Router>
     </div>
 
